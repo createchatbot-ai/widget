cf3e75cd71c3d187eb26be6e3d89458fb33a0490
Shiraz Khan <147941812+skccai@users.noreply.github.com>
2023-11-01 18:23:03 +0100

    change config

--- packages/core/stencil.config.ts
+++ packages/core/stencil.config.ts
@@ -8,9 +8,6 @@ export const config: Config = {
       esmLoaderPath: '../loader',
     },
     {
-      type: 'dist-custom-elements',
-    },
-    {
       type: 'www',
       serviceWorker: null, // disable service workers
     },
