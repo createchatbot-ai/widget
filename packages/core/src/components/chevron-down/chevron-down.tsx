96ac4681b1ae71128ae85822f130beedf629935e
Shiraz Khan <147941812+skccai@users.noreply.github.com>
2023-11-01 23:53:04 +0100

    [Core] Fix SVG Style

--- packages/core/src/components/chevron-down/chevron-down.tsx
+++ packages/core/src/components/chevron-down/chevron-down.tsx
@@ -9,7 +9,7 @@ export class ChevronDown {
 
   render() {
     return (
-      <svg style={{ width: '42px', marginTop: '5px' }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke={this.stroke} class="w-6 h-6">
+      <svg style={{ width: '42px', marginTop: '8px' }} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke={this.stroke} class="w-6 h-6">
         <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
       </svg>
     );
